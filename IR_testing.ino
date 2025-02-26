d14ec42eb80e7b3eb2ef2afb6fd1343b2d15c31c
Beren Eskildsen <besk864@aucklanduni.ac.nz>
2025-02-26 15:24:49 +1300

    Changed reading analog of signal to digital and reading speed increased significantly

--- IR_testing.ino
+++ IR_testing.ino
@@ -1,31 +1,32 @@
 #include <Arduino_BuiltIn.h>
 
 
-// #define arraySize 2000
+#define arraySize 2000
 
-// int timerStatus = 0;
-// int doubleCheck = 3;
-// int countDown = 50;
-// volatile int sensorValue;
-// volatile uint8_t voltage = 0;
-// volatile int j = 0;
+int timerStatus = 0;
+int doubleCheck = 3;
+int countDown = 50;
+volatile int sensorValue;
+volatile uint8_t voltage = 0;
+volatile int j = 0;
 volatile float counter = 0;
 
-// uint8_t infraArray[arraySize];
+uint8_t infraArray[arraySize];
 
 void timerInit() {
   TCCR1A = 0;
 
-  OCR1A = 5000;
+  OCR1A = 200;
 
   TIMSK1 = (1 << OCIE1A);
 
-  TCCR1B = (1 << WGM12) | (1 << CS10);
+  TCCR1B = (1 << WGM12);
 }
 
 void setup() {
 
   pinMode(11, OUTPUT);
+  pinMode(10, INPUT);
 
   Serial.begin(9600);
   while (!Serial) ;
@@ -38,36 +39,29 @@ void setup() {
 
 ISR(TIMER1_COMPA_vect) {
 
-  Serial.println(counter);
-  // Serial.print("A");
-  counter = 0;
-  // sensorValue = analogRead(A0);
+  voltage = digitalRead(10);
   // voltage = sensorValue * (5.0 / 1023.0);
 
-  // if (j < arraySize) {
-  //   infraArray[j] = voltage;
-  //   j++;
-  // }
-  // Serial.print(voltage);
-  // Serial.print(" ");
+  if (j < arraySize) {
+    infraArray[j] = voltage;
+    j++;
+  }
 
 }
 
 void loop() {
-  digitalWrite(11, HIGH);
-  counter += 0.01;
-  // if (timerStatus == 0) {
-  // counter += 0.01;
-  //   sensorValue = analogRead(A0);
-  //   voltage = sensorValue * (5.0 / 1023.0);
+  if (timerStatus == 0) {
 
+    voltage = digitalRead(10);
+    // voltage = sensorValue * (5.0 / 1023.0);
 
-  //   if (voltage < 4) {
-  //     timerStatus = 1;
-  //     TCCR1B |= (1 << CS11);  // Start timer, prescaler 8
-  //   }
+    if (voltage == 0) {
+      timerStatus = 1;
+      OCR1A = 200;
+      TCCR1B = (1 << WGM12) | (1 << CS10);  // Start timer
+    }
 
-  // }
+  }
 
   // if (number == 1) {
 
@@ -83,17 +77,17 @@ void loop() {
 
   // }
 
-  // if ((j == arraySize) && (doubleCheck == 3)) {
+  if ((j == arraySize) && (doubleCheck == 3)) {
 
-  //   TCCR1B &= ~(1 << CS11); // Stop timer
-  //   doubleCheck = 1;
+    TCCR1B &= ~(1 << CS10); // Stop timer
+    doubleCheck = 1;
 
-  //   for (int k = 0; k < arraySize; k++) {
-  //     Serial.print(infraArray[k]);
-  //     Serial.print(" ");
-  //   }
+    for (int k = 0; k < arraySize; k++) {
+      Serial.print(infraArray[k]);
+      Serial.print(" ");
+    }
 
-  // }
+  }
 
 }
 
